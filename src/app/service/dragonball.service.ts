7eb570e9fb278a39be25d2a36f964823a5961efb
ROBERTO TIRON <robtiron@emeal.nttdata.com>
2025-05-28 18:01:13 +0200

    implemented service to save into localStorage

--- src/app/service/dragonball.service.ts
+++ src/app/service/dragonball.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, signal, WritableSignal} from '@angular/core';
+import {effect, Injectable, signal, WritableSignal} from '@angular/core';
 import {Character} from '../interface/character.interface';
 
 @Injectable({providedIn: 'root'})
@@ -6,13 +6,29 @@ export class DragonBallService {
 
   //servicio que gestionara la data de los personajes
 
-  characters: WritableSignal<Character[]> = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9000},
-    {id: 2, name: 'Vegeta', power: 8500},
-  ]);
+  characters: WritableSignal<Character[]> = signal<Character[]>(
+    this.loadFromLocalStorage()
+  );
 
-  addCharacter(character: Character):void {
-    this.characters.update((actual)=> [...actual, character]);
+  //el effecto lo que hace es saber el codigo que ejecuta dentro y si la senal que tiene dentro cambia, entonces se dispara
+  saveToLocalStorage = effect(() => {
+
+    localStorage.setItem('characters', JSON.stringify(this.characters())); //detecta esta senal cuando cambia
+
+  });
+
+  loadFromLocalStorage(): [] {
+    const characters = localStorage.getItem('characters');
+
+    if (characters && characters !== 'undefined') {
+      return JSON.parse(characters);
+    }
+    return [];
+
+  }
+
+  addCharacter(character: Character): void {
+    this.characters.update((actual) => [...actual, character]);
   }
 
 
